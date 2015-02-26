9ff95bf469ad593a767b56cb2f05ebda0d89b2f5
Owen Peterson <owen.a.peterson@gmail.com>
2015-02-26 12:35:27 -0600

    Android Studio Git Integration test.

--- app/src/main/java/ca/owenpeterson/rssreader/common/Detailable.java
+++ app/src/main/java/ca/owenpeterson/rssreader/common/Detailable.java
@@ -6,6 +6,8 @@ import org.joda.time.DateTime;
  * Created by Owen on 2/24/2015.
  *
  * Interface used to define the contract for an item that is going to be displayed in the DetailActivity.
+ *
+ * Adding this coment to test the Git Integration.
  */
 public interface Detailable {
 
